7852e6dbd1b1eb6291f859e9ee2e51109a7b1cea
Erika Barreto <sancdani01@gmail.com>
2024-04-15 15:53:43 -0600

    codigo corregido 2

---
+++ formulario.js
@@ -0,0 +1,72 @@
+var formulario = document.querySelector("#form");
+
+formulario.onsubmit = function(e) {
+  e.preventDefault(); // Evita que el formulario se envíe automáticamente
+
+  var nombreInput = formulario.elements[0];
+  var edadInput = formulario.elements[1];
+  var nacionalidadSelect = formulario.elements[2];
+
+  var nombre = nombreInput.value;
+  var edad = parseInt(edadInput.value); // Convertir a entero
+  var i = nacionalidadSelect.selectedIndex;
+  var nacionalidad = nacionalidadSelect.options[i].value;
+
+  console.log(nombre, edad);
+  console.log(nacionalidad);
+
+  if (nombre.length === 0) {
+    nombreInput.classList.add("error");
+  }
+  if (edad < 18 || edad > 120) {
+    edadInput.classList.add("error");
+  }
+
+  if (nombre.length > 0 && edad >= 18 && edad <= 120) {
+    agregarInvitado(nombre, edad, nacionalidad);
+  }
+};
+
+function agregarInvitado(nombre, edad, nacionalidad) {
+  if (nacionalidad === "ar") {
+    nacionalidad = "Argentina";
+  } else if (nacionalidad === "mx") {
+    nacionalidad = "Mexicana";
+  } else if (nacionalidad === "vnzl") {
+    nacionalidad = "Venezolana";
+  } else if (nacionalidad === "per") {
+    nacionalidad = "Peruana";
+  }
+
+  var lista = document.getElementById("lista-de-invitados");
+
+  var elementoLista = document.createElement("div");
+  elementoLista.classList.add("elemento-lista");
+  lista.appendChild(elementoLista);
+
+  function crearElemento(descripcion, valor) {
+    var spanNombre = document.createElement("span");
+    var inputNombre = document.createElement("input");
+    var espacio = document.createElement("br");
+    spanNombre.textContent = descripcion + ": ";
+    inputNombre.value = valor;
+    elementoLista.appendChild(spanNombre);
+    elementoLista.appendChild(inputNombre);
+    elementoLista.appendChild(espacio);
+  }
+
+  crearElemento("Nombre", nombre);
+  crearElemento("Edad", edad);
+  crearElemento("Nacionalidad", nacionalidad);
+
+  var botonBorrar = document.createElement("button");
+  botonBorrar.textContent = "Eliminar invitado";
+  botonBorrar.id = "boton-borrar";
+  var corteLinea = document.createElement("br");
+  elementoLista.appendChild(corteLinea);
+  elementoLista.appendChild(botonBorrar);
+
+  botonBorrar.onclick = function() {
+    elementoLista.remove();
+  };
+}
